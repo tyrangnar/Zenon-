997a52dfef1eca7c7957c5ce7b60a4ef9ce49fd3
tyrangnar <erikson.coronel@gmail.com>
2025-07-23 20:01:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-const lista = document.getElementById("inversiones");
-
-function agregarInversion() {
-    const nombre = document.getElementById("nombre").value;
-    const monto = document.getElementById("monto").value;
-    if (!nombre || !monto) return alert("Completa todos los campos");
-
-    const li = document.createElement("li");
-    li.textContent = `${nombre}: ${monto}`;
-    lista.appendChild(li);
-
-    document.getElementById("nombre").value = "";
-    document.getElementById("monto").value = "";
-}
+const toggleButton = document.getElementById('theme-toggle');
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+});
